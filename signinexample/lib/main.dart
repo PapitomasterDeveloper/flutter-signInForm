f7715f33683ac188d204255c757ba12f63c59b30
Miguel Angel Cavazos Reyes <miguel.cavazos@ad.infosys.com>
2020-11-20 17:20:14 -0600

    Enabled a fillment process form logic

--- signinexample/lib/main.dart
+++ signinexample/lib/main.dart
@@ -58,9 +58,29 @@ class _SignUpFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/welcome');
   }
 
+void _updateFormProgress() {
+  var progress = 0.0;
+  var controllers = [
+    _firstNameTextController,
+    _lastNameTextController,
+    _usernameTextController
+  ];
+
+  for (var controller in controllers) {
+    if (controller.value.text.isNotEmpty) {
+      progress += 1 / controllers.length;
+    }
+  }
+
+  setState(() {
+    _formProgress = progress;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -99,7 +119,7 @@ class _SignUpFormState extends State<SignUpForm> {
                 return states.contains(MaterialState.disabled) ? null : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: Text('Sign up'),
           ),
         ],
